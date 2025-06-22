031d772b4d7a124214cb99355676667d109a58ec
James Kalu <jamesiukalu@gmail.com>
2025-06-22 14:43:58 -0400

    fix

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5001/api',https://financial-track-l5utci8pv-james-kalus-projects.vercel.app/api
-  baseURL: import.meta.env.VITE_BACKEND_URL || 'https://financial-track-l5utci8pv-james-kalus-projects.vercel.app/api',
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'https://financial-track-api.vercel.app/api',
   withCredentials: true,
 });
 
