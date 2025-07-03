1b53f508e11698d06fc0aa48f2a0e48b575540e5
James Kalu <jamesiukalu@gmail.com>
2025-07-02 20:43:59 -0400

    update

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5001/api',
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'https://finanacetrackerapi-production.up.railway.app/api',
   //baseURL: import.meta.env.VITE_BACKEND_URL || 'https://finanacetrackerapi-production.up.railway.app/api',
   withCredentials: true,
 });
